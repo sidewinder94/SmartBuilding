478050bbeeb4aeff439c8936d837df2d89367b15
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2015-10-05 13:51:03 +0200

    Loop Animation

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -13,7 +13,8 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         var chr = GetComponent<CharacterController>();
-        chr.Move(new Vector3(0.0f, 0.0f, 0.1f));
+        var rb = GetComponent<Rigidbody>();
+        chr.Move(rb.position.z > 10 ? new Vector3(0.0f, 0.0f, -20.0f) : new Vector3(0.0f, 0.0f, 0.1f));
     }
 
 }
