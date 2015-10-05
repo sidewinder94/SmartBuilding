aeb44d222b8d33ffd83f1e5a3d90d290636bbe65
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2015-10-05 13:24:44 +0200

    Initial Commit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var chr = GetComponent<CharacterController>();
+        chr.Move(new Vector3(0.0f, 0.0f, 0.1f));
+    }
+
+}
---
+++ Assets/RoomScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log(collision.name + " collided with me");
+    }
+
+    void OnTriggerStay(Collider collision)
+    {
+        Debug.Log(collision.name + " is within my boundaries");
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        Debug.Log(collision.name + " exited my boundaries");
+    }
+}
