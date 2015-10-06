920ffc5e860bff7729e8103c5048335d3f52a21e
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2015-10-07 01:57:08 +0200

    Haters gonna Heat (We don't support cooling yet, don't abuse)

--- Assets/HeatPumpScript.cs
+++ Assets/HeatPumpScript.cs
@@ -1,29 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using Object = UnityEngine.Object;
 
 public class HeatPumpScript : MonoBehaviour
 {
 
-    public float AvailablePower = 9.0f;
+    public double TotalPower = 9.0f;
 
     private readonly Mutex _mutex = new Mutex();
-    private readonly Dictionary<Object, float> _allocatedPower = new Dictionary<Object, float>();
+    private readonly Dictionary<Object, double> _allocatedPower = new Dictionary<Object, double>();
 
     public HeatPumpScript()
     {
         UsedPower = 0.0f;
     }
 
-    public float UsedPower { get; private set; }
+    public double UsedPower { get; private set; }
+    public double ActualPower { get { return TotalPower - UsedPower; } }
 
-
-    public bool AskPower(float required, Object sender)
+    public double AskPower(double required, Object sender)
     {
-        var wasAvailable = false;
+        double allocated;
         _mutex.WaitOne();
-        if (UsedPower + required <= AvailablePower)
+        if (UsedPower + required <= TotalPower)
         {
             if (!_allocatedPower.ContainsKey(sender))
             {
@@ -35,18 +37,42 @@ public class HeatPumpScript : MonoBehaviour
             }
 
             UsedPower += required;
-            wasAvailable = true;
+            allocated = required;
+        }
+        else
+        {
+            allocated = TotalPower - UsedPower;
+            UsedPower = TotalPower;
         }
         _mutex.ReleaseMutex();
-        return wasAvailable;
+        return allocated;
     }
 
-    public void ReleasePower(float used, Object sender)
+    public void ReleasePower(double used, Object sender)
     {
         _mutex.WaitOne();
+        if (_allocatedPower.ContainsKey(sender))
+        {
+            double val = _allocatedPower[sender];
+            if (used > val)
+            {
+                throw new UnauthorizedAccessException();
+            }
 
+            val -= used;
+            UsedPower -= used;
 
+            if (val == 0)
+            {
+                _allocatedPower.Remove(sender);
+            }
+        }
+        else
+        {
+            throw new UnauthorizedAccessException();
+        }
 
+        _mutex.ReleaseMutex();
 
     }
 
--- Assets/RoomScript.cs
+++ Assets/RoomScript.cs
@@ -5,18 +5,25 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using JetBrains.Annotations;
+using UnityEditor;
 
 public class RoomScript : MonoBehaviour, INotifyPropertyChanged
 {
-    public float TargetTemperature = 20.0f;
-    public float CurrentTemperature = 15.0f;
-    public float RoomSize = 0.0f;
+    public double TargetTemperature = 20.0f;
+    public double CurrentTemperature = 15.0f;
+    public double AllowedDeltaTemp = 0.2f;
+    public double RoomSize = 0.0f;
 
     private List<GameObject> _roomLights;
     private PlayerScript _player = null;
     private HeatPumpScript _heatPump = null;
     private OutsideScript _outside = null;
+    private List<GameObject> _heaters;
     private Boolean _lighted = false;
+    private double _neededHeatPower;
+    private double _allocatedPower;
+    private double _powerLoss;
+
 
     public Boolean Lighted
     {
@@ -34,6 +41,7 @@ public class RoomScript : MonoBehaviour, INotifyPropertyChanged
     {
         _heatPump = GameObject.FindGameObjectWithTag("HeatPump").GetComponent<HeatPumpScript>();
         _roomLights = GameObject.FindGameObjectsWithTag(tag).Where(o => o.layer == 8).ToList();
+        _heaters = GameObject.FindGameObjectsWithTag(tag).Where(o => o.layer == 9).ToList();
         _outside = GameObject.FindGameObjectWithTag("Terrain").GetComponent<OutsideScript>();
     }
 
@@ -44,7 +52,66 @@ public class RoomScript : MonoBehaviour, INotifyPropertyChanged
 
     void FixedUpdate()
     {
-        var dt = Time.fixedDeltaTime;
+
+
+        if (_allocatedPower > 0)
+        {
+            //Fin de calcul du round précédent => calcul de la vitesse de changement de temp et application de la nouvelle temp.
+            double neededTime = _neededHeatPower / _allocatedPower;
+            double heatingSpeed = (TargetTemperature - CurrentTemperature) / neededTime;
+            double diffTemp = heatingSpeed * Time.fixedDeltaTime;
+            CurrentTemperature += diffTemp;
+        }
+        else
+        {
+
+        }
+
+
+        //Calcul de la déperdition de chaleur
+        double powerLoss = RoomSize * 0.75f * (CurrentTemperature - _outside.Temperature);
+        double neededPower = 0.0f;
+
+        //Si on est dans la fourchette autorisée de la température requise
+        if (TargetTemperature - CurrentTemperature < AllowedDeltaTemp)
+        {
+            //On ne demande que la puissance requise pour compense la déperdition
+            neededPower = powerLoss;
+        }
+        else
+        {
+            //Sinon et si on doit chauffer (Target > Current)
+            if (powerLoss > 0)
+            {
+                //Calcul de la masse volumique de l'air
+                double rho = (1.0f / (287.06f * (CurrentTemperature + 273.15f))) * (_outside.Pressure - 230.617 * _outside.Humidity * Math.Exp((17.503 * CurrentTemperature) / (241.2 + CurrentTemperature)));
+                //Calcul de la masse d'air dans la pièce 
+                double airMass = RoomSize * rho;
+                //Calcul de la puissance nécessaire pour chauffer le volume d'air
+                neededPower = airMass * (TargetTemperature - CurrentTemperature);
+                //On ajoute la déperdition de chaleur
+                neededPower += powerLoss;
+                //On définit la puissance requise pour monter a la température voulue pour le calcul au prochain round
+                _neededHeatPower = neededPower;
+            }
+            else
+            {
+                //Si on a trop chaud, on relache toutes nos ressources pour tenter de profiter de la déperdition de chaleur pour faire baisser la température
+                _heatPump.ReleasePower(_allocatedPower, this);
+                _allocatedPower = 0.0f;
+            }
+        }
+
+        //Si on a plus de ressources que nécessaire, on les relâches
+        if (_allocatedPower > neededPower)
+        {
+            _heatPump.ReleasePower(_allocatedPower - neededPower, this);
+        }
+        else
+        {
+            //Sinon on demande ce qui nous manque à la pompe (elle donne ce qu'elle peut)
+            _allocatedPower += _heatPump.AskPower(neededPower - _allocatedPower, this);
+        }
     }
 
     void OnTriggerEnter(Collider collision)
