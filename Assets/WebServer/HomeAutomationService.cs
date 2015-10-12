4294c3ce874a9344b78c86d2453d86e2a64e7af4
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2015-10-12 12:24:09 +0200

    Created WebService

---
+++ Assets/WebServer/HomeAutomationService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.WebServer
+{
+    class HomeAutomationService : IHomeAutomationService
+    {
+        private static readonly List<String> Rooms = new List<string>
+        {
+            "HeatPump",
+            "Terrain",
+            "LivingRoom",
+            "WC",
+            "Room1",
+            "Cellar",
+            "Garage",
+            "Room2",
+            "Room3",
+            "Room4",
+            "WCUpstairs",
+            "UpstairsLane",
+            "UpstairsBathroom"
+        };
+
+        public List<int> GetRoomList()
+        {
+            return Enumerable.Range(0, Rooms.Count).ToList();
+        }
+
+        public String GetRoomName(int roomId)
+        {
+            return roomId < Rooms.Count ? Rooms[roomId] : "Invalid ID";
+        }
+
+        public List<string> GetAvailableProperties(int roomId)
+        {
+            if (roomId > Rooms.Count) return new List<string>();
+            return roomId <= 1 ? new List<string>() : new List<string> { "TargetTemperature" };
+        }
+
+        public bool SetRoomProperty(int roomId, string property, string value)
+        {
+            if (roomId > Rooms.Count) return false;
+
+            var roomName = Rooms[roomId];
+
+            var props = GetAvailableProperties(roomId);
+
+            if (!props.Contains(property)) return false;
+
+            Double dble;
+
+            if (!Double.TryParse(value, out dble)) return false;
+
+            GameObject.Find(roomName).GetComponent<RoomScript>().TargetTemperature = dble;
+
+            return true;
+        }
+
+    }
+}
---
+++ Assets/WebServer/IHomeAutomationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Assets.WebServer
+{
+    [ServiceContract]
+    interface IHomeAutomationService
+    {
+        [OperationContract]
+        List<int> GetRoomList();
+
+
+        [OperationContract]
+        String GetRoomName(int roomId);
+
+        [OperationContract]
+        List<string> GetAvailableProperties(int roomId);
+
+        [OperationContract]
+        bool SetRoomProperty(int roomId, string property, string value);
+    }
+}
